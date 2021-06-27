5d04be936da046a2475e7bbf3325f31c59de317b
Michael Degginger <michael.degginger@phoenix.edu>
2021-06-27 09:59:04 -0600

    paginationstart

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,16 +1,34 @@
 import styles from "../../styles/Feed.module.css";
+import { useRouter } from "next/router";
 
 export const Feed = ({ pageNumber, articles }) => {
+  const router = useRouter();
+
   return (
+    <div className="page-container">
       <div className={styles.main}>
-          {articles.map((article, index) => (
-            <div key={index} className={styles.post}>
-                <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
-                <p>{article.description}</p>
-                {!!article.urlToImage && <img src= {article.urlToImage} />}
-            </div>
-          ))}
+        {articles.map((article, index) => (
+          <div key={index} className={styles.post}>
+            <h1 onClick={() => (window.location.href = article.url)}>
+              {article.title}
+            </h1>
+            <p>{article.description}</p>
+            {!!article.urlToImage && <img src={article.urlToImage} />}
+          </div>
+        ))}
       </div>
+      <div className={styles.paginator}>
+        <div
+          onClick={() => {
+            if (pageNumber > 1) {
+              router.push(`/feed/${pageNumber -1}`).then(() => window.scroll(0,0));
+            }
+          }}
+          className={pageNumber === 1 ? styles.disabled : styles.active}
+        >Previous Page</div>
+        <div>#{pageNumber}</div>
+      </div>
+    </div>
   );
 };
 
